82b45b3372ae688a90180b7fb93097d3f896c699
Vaish <v.logendra1993@gmail.com>
2026-03-10 14:15:02 +0100

    clouds and character animated

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,20 @@
 class Cloud extends MoveableObject{
+  y = 50;
+  width= 500;
+  height = 300;
+
+
   constructor() {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
 
     this.x = 200 + Math.random() * 500;
-    this.y = 20;
-    this.height = 300;
-    this.width = 500;
-  }
+    this.animate();
+}
+
+animate() {
+  setInterval(() => {
+    this.x -= 0.3;
+  }, 1000/60);
+}
+
 }
--- models/moveable-object.classes.js
+++ models/moveable-object.classes.js
@@ -1,23 +1,29 @@
 class MoveableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
+  x = 120;
+  y = 280;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = {};
 
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 
+  moveRight() {
+    console.log("Moving right");
+  }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    moveRight() {
-        console.log('Moving right');
-    }
-
-    moveleft(){
-console.log('Moving left');
-    }
+  moveleft() {
+    console.log("Moving left");
+  }
 }
