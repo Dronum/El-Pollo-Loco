1eb1f36d162cad08ec2ef161b02a15e4454d68ca
Vaish <v.logendra1993@gmail.com>
2026-03-10 17:37:33 +0100

    chicken animated

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,20 +1,19 @@
-class Cloud extends MoveableObject{
+class Cloud extends MoveableObject {
   y = 50;
-  width= 500;
+  width = 500;
   height = 300;
-
+  
 
   constructor() {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
 
     this.x = 200 + Math.random() * 500;
     this.animate();
-}
+  }
 
-animate() {
-  setInterval(() => {
-    this.x -= 0.3;
-  }, 1000/60);
-}
+  animate() {
+    this.moveleft();
+  }
+
 
 }
