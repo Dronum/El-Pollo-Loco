8a519c148e2fc81db5a2bef678d051a1526fd637
Vaish <v.logendra1993@gmail.com>
2026-03-11 21:43:04 +0100

    colision detection

--- models/world.class.js
+++ models/world.class.js
@@ -1,7 +1,6 @@
 import { Character } from "./character.class.js";
 import { level1 } from "../js/levels/level1.js";
 
-
 export class World {
   character = new Character();
   level = level1;
@@ -53,6 +52,11 @@ export class World {
       mo.x = mo.x * -1;
     }
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    this.ctx.beginPath();
+    this.ctx.lineWidth = "5";
+    this.ctx.strokeStyle = "blue";
+    this.ctx.rect(mo.x, mo.y, mo.x, +mo.width, mo.y, mo.height);
+    this.ctx.stroke();
     if (mo.otherDirection) {
       mo.x = mo.x * -1;
       this.ctx.restore();
