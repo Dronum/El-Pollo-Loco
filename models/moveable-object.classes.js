916a308e2adb72cf687ab643810813dbe618b73c
Vaish <v.logendra1993@gmail.com>
2026-03-12 11:36:46 +0100

    colliding

--- models/moveable-object.classes.js
+++ models/moveable-object.classes.js
@@ -40,6 +40,13 @@ export class MoveableObject {
     ctx.stroke();
   }
 
+  isColliding (mo) {
+    return this.x + this.width > mo.x &&
+    this.y + this.height > mo.y &&
+    this.x < mo.x &&
+    this.y < mo.y + mo.height;
+  }
+
   loadImages(arr) {
     arr.forEach((path) => {
       let img = new Image();
